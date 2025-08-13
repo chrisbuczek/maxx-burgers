29a36fdabeb3d97e3efba5a66f319eaf5c432249
chrisbuczek <krzysztofbuczek@live.com>
2025-08-14 00:11:23 +0200

    change eslint.config to javascript

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,4 +1,14 @@
-import config from "eslint-config-xo";
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig([config]);
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+]);
