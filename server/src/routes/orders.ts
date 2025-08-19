166b1d064b9cec6b32b0638979f064f2c0ce7fc3
chrisbuczek <krzysztofbuczek@live.com>
2025-08-19 23:38:12 +0200

    add product crud routes

---
+++ server/src/routes/orders.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({ message: "List of users" });
+});
+
+router.get("/create", (req, res) => {
+  res.json({ message: "List of users" });
+});
+
+export default router;
---
+++ server/src/routes/products.ts
@@ -0,0 +1,23 @@
+import express from "express";
+const router = express.Router();
+import Product from "../models/Product.js";
+
+router.get("/", async (req, res) => {
+  const products = Product.find({});
+  res.json({ products });
+});
+
+router.post("/create", (req, res) => {
+  res.json({ message: "List of users" });
+});
+
+router.put("/update/:id", (req, res) => {
+  res.json({ message: "List of users" });
+});
+
+router.delete("/delete/:id", async (req, res) => {
+  const deletedCount = await Product.deleteOne({ _id: req.params.id });
+  res.json({ message: deletedCount });
+});
+
+export default router;
