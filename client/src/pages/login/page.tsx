e8153045bbb2ad6f6b4bf91279cfdd1404577db3
chrisbuczek <krzysztofbuczek@live.com>
2025-09-24 23:48:26 +0200

    login add use-form-hook validation

--- client/src/pages/login/page.tsx
+++ client/src/pages/login/page.tsx
@@ -1,3 +1,5 @@
+import { useForm } from "react-hook-form";
+import type { SubmitHandler } from "react-hook-form";
 import TextField from "@mui/material/TextField";
 import SecondNavbar from "../../components/SecondNavbar/SecondNavbar";
 
@@ -8,44 +10,60 @@ const Metadata = () => (
   </>
 );
 
+type Inputs = {
+  email: string;
+};
+
 const Page = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <>
       <Metadata />
       <SecondNavbar />
       <div className="p-6">
         <div className="py-6 font-bold">Please fill in</div>
-        <TextField
-          id="filled-email"
-          label="E-mail address"
-          variant="filled"
-          sx={{
-            "& .MuiInputLabel-root": {
-              color: "red",
-              transform: "translate(12px, 16px) scale(1)",
-              transformOrigin: "top left",
-              transition: "transform 0.3s ease, color 0.3s ease",
-              "&.Mui-focused": {
-                color: "gray",
-                transform: "translate(4px, -18px) scale(0.75)",
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <TextField
+            {...register("email", { required: true })}
+            id="filled-email"
+            label="E-mail address"
+            variant="filled"
+            error={!!errors.email}
+            helperText={errors.email && "This fiels is required"}
+            sx={{
+              "& .MuiInputLabel-root": {
+                color: "red",
+                transform: "translate(12px, 16px) scale(1)",
+                transformOrigin: "top left",
+                transition: "transform 0.3s ease, color 0.3s ease",
+                "&.Mui-focused": {
+                  color: "gray",
+                  transform: "translate(4px, -18px) scale(0.75)",
+                },
+                "&.MuiFormLabel-filled": {
+                  transform: "translate(4px, -18px) scale(0.75)",
+                },
               },
-              "&.MuiFormLabel-filled": {
-                transform: "translate(4px, -18px) scale(0.75)",
+              "& .MuiFilledInput-root": {
+                "&:before": {
+                  borderBottom: "none",
+                },
+                "&:hover:not(.Mui-disabled):before": {
+                  borderBottom: "none",
+                },
+                "&:after": {
+                  borderBottom: "2px solid gray",
+                },
               },
-            },
-            "& .MuiFilledInput-root": {
-              "&:before": {
-                borderBottom: "none",
-              },
-              "&:hover:not(.Mui-disabled):before": {
-                borderBottom: "none",
-              },
-              "&:after": {
-                borderBottom: "2px solid gray",
-              },
-            },
-          }}
-        />
+            }}
+          />
+        </form>
       </div>
     </>
   );
