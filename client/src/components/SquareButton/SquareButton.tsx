e105742ab6e492ddaf5e0d0aa3d7925feea3f97e
chrisbuczek <krzysztofbuczek@live.com>
2025-09-25 23:58:38 +0200

    update SquareButton

--- client/src/components/SquareButton/SquareButton.tsx
+++ client/src/components/SquareButton/SquareButton.tsx
@@ -1,17 +1,31 @@
 import React from "react";
+import { IoIosArrowBack } from "react-icons/io";
+import { useTranslation } from "react-i18next";
 
 interface ISquareButton {
   children: React.ReactNode;
+  className?: string | "";
 }
 
-export const SquareButton: React.FC<ISquareButton> = ({ children }) => {
+export const SquareButton: React.FC<ISquareButton> = ({
+  children,
+  className,
+}) => {
   return (
-    <button className="bg-transparent border-2 border-maxred rounded-2xl">
+    <button
+      className={`bg-transparent border-3 border-maxred text-maxred rounded-[0.5rem] w-22 h-22 flex flex-col items-center justify-center ${className}`}
+    >
       {children}
     </button>
   );
 };
 
 export const SquareButtonBack = () => {
-  return <SquareButton>Back</SquareButton>;
+  const { t } = useTranslation();
+  return (
+    <SquareButton>
+      <IoIosArrowBack size={30} />
+      {t("back")}
+    </SquareButton>
+  );
 };
