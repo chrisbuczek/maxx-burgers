ae57957f696bbfd6a0bdef512cd6eca7427080b2
chrisbuczek <krzysztofbuczek@live.com>
2025-10-02 05:57:24 +0200

    fix main page

--- client/src/pages/page.tsx
+++ client/src/pages/page.tsx
@@ -25,10 +25,10 @@ function Page() {
           desktopData={videoCarouselDesktopData}
         />
         <div className="w-full h-[500px] bg-maxbeige z-10">
-          <div className="bg-maxbeige flex flex-col items-center">
+          <div className="bg-maxbeige flex flex-col items-center px-2 md:px-0">
             <div className="md:hidden w-0 h-0 border-l-[10px] border-r-[10px] border-b-[10px] border-l-transparent border-r-transparent translate-y-[-100%] border-b-maxbeige"></div>
             <ThreeButtons className="relative md:translate-y-[-50%]" />
-            <div className="mt-1 flex flex-col gap-20">
+            <div className="mt-10 md:mt-0 flex flex-col gap-15 md:translate-y-[-20%]">
               <ChooseCup />
               <ChooseLanguage />
             </div>
